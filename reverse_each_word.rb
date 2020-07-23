4f656601c94713a321312aef0ae059c3534e6b94
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-23 16:24:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(string)
   reverse_array= []
   reverse_string=""
  string_array.each do |element|
-  reverse_array << element.reverse 
+  puts reverse_array << element.reverse 
   reverse_string = reverse_array.join 
   end
   reverse_string
