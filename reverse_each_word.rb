7a3e569e4e445dfdf362817ad083b7e6a67ab449
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-23 16:28:09 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+def reverse_each_word(string)
+  string_array = string.split
+  reverse_array= []
+  reverse_string=""
+ string_array.collect do |element|
+  reverse_array << element.reverse 
+  reverse_string = reverse_array.join(" ") 
+  end
+  reverse_string
+end
+
+string ="Hello there, and how are you?"
+reverse_each_word(string)
